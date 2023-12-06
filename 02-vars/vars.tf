294995fc16fdbbbb0b09d3b9ce664ed5cdffb337
Anto <japanto20@gmail.com>
2023-12-06 17:30:17 -0600

    Create servers

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,11 @@ variable "sample1" {
 output "sample2" {
   value = var.sample1
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruits_first_fruit" {
+  value = var.fruits[0]
+}
