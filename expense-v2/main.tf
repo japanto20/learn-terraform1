4d0cff78b23c1a393a8b8fa8bcecd18f8020aa2a
Anto <japanto20@gmail.com>
2023-12-06 22:35:15 -0600

    Create servers

--- expense-v2/main.tf
+++ expense-v2/main.tf
@@ -1,49 +1,49 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.sg_id
   tags = {
-    Name = "frontend"
+    Name = "frontend-${var.env}"
   }
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z07168053N2UH00BX82ND"
-  name    = "frontend-dev"
+  zone_id =  var.zone_id
+  name    = "frontend-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
 
 resource "aws_instance" "backend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.sg_id
   tags = {
-    Name = "backend"
+    Name = "backend-${var.env}"
   }
 }
 
 resource "aws_route53_record" "backend" {
-  zone_id = "Z07168053N2UH00BX82ND"
-  name    = "backend-dev"
+  zone_id = var.zone_id
+  name    = "backend-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.backend.private_ip]
 }
 
 resource "aws_instance" "mysql" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.sg_id
   tags = {
-    Name = "mysql"
+    Name = "mysql-${var.env}"
   }
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z07168053N2UH00BX82ND"
-  name    = "mysql-dev"
+  zone_id =  var.zone_id
+  name    = "mysql-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
