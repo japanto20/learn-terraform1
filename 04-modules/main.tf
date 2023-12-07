347105e832e72e9dcc2f48ff8bae0bd91f551980
Anto <japanto20@gmail.com>
2023-12-07 14:19:30 -0600

    Create servers

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,3 +1,12 @@
 module "test" {
-  source = "./test"
+  for_each = var.instances
+  source   = "./test"
+}
+
+variable "instances" {
+  default = {
+    frontend = {}
+    backend  = {}
+    mysql    = {}
+  }
 }
