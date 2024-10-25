cea4830c09f0cc46b37f776e8b0a3a74c7bb0669
Anto <japanto20@gmail.com>
2024-10-24 20:06:37 -0500

    frontend checking

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,5 +1,9 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "frontend"
+  }
 
 }
